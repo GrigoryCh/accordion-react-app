70fa42d72e452f906d71f8813994768c646871c7
GrigoryCh <91157056+GrigoryCh@users.noreply.github.com>
2022-03-18 11:26:32 +0300

    Add files via upload

---
+++ precache-manifest.e4bc7aab46b5e97179a2f34266c949c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e23e560e5d62c0d6b2e61667642014d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1963ab48f93d4230126b",
+    "url": "/static/css/main.ebc55513.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1963ab48f93d4230126b",
+    "url": "/static/js/main.79e5d3ef.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
